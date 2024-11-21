d40c7745c8b6dc0a54546a018956cc996d7e4ecb
Leeiss <farrakhova.2004@mail.ru>
2024-11-21 23:46:02 +0300

    3

--- lab3/mo3Bar.py
+++ lab3/mo3Bar.py
@@ -3,7 +3,7 @@ from tabulate import tabulate
 
 class CONSTANTS:
     eps = 0.002
-    r_0 = 5.0 # значение параметра штрафа 
+    r_k = 5.0 # значение параметра штрафа 
     C = 4 # для увеличения параметра штрафа
     alpha = 0.1
     beta = 0.8
@@ -20,31 +20,31 @@ def gradient(func, x, y):
     dfdy = (func(x, y + h) - func(x, y - h)) / (2 * h)
     return [dfdx, dfdy]
 
-def backtracking_line_search(func, x_k, grad, initial_lambda=1.0):
+def backtracking_line_search(func, x_dash_k, grad, initial_lambda=1.0):
     lam = initial_lambda
-    f_x_k = func(*x_k)
+    f_x_dash_k = func(*x_dash_k)
     while True:
-        x_k_new = vector_sub(x_k, vector_mul(grad, lam))
-        f_x_k_new = func(*x_k_new)
+        x_dash_k_new = vector_sub(x_dash_k, vector_mul(grad, lam))
+        f_x_dash_k_new = func(*x_dash_k_new)
         grad_norm_sq = sum(g**2 for g in grad)
 
-        if f_x_k_new <= f_x_k - CONSTANTS.alpha * lam * grad_norm_sq:
+        if f_x_dash_k_new <= f_x_dash_k - CONSTANTS.alpha * lam * grad_norm_sq:
             break
 
         lam *= CONSTANTS.beta
     return lam
 
 def cauchy_method(func, x):
-    x_k = x[:]
+    x_dash_k = x[:]
     itr = 0
     while itr < 100:
-        grad = gradient(func, x_k[0], x_k[1])
-        lam = backtracking_line_search(func, x_k, grad)
-        x_k_next = vector_sub(x_k, vector_mul(grad, lam))
+        grad = gradient(func, x_dash_k[0], x_dash_k[1])
+        lam = backtracking_line_search(func, x_dash_k, grad)
+        x_dash_k_next = vector_sub(x_dash_k, vector_mul(grad, lam))
 
-        if abs(func(*x_k_next) - func(*x_k)) <= CONSTANTS.eps:
-            return x_k_next
-        x_k = x_k_next
+        if abs(func(*x_dash_k_next) - func(*x_dash_k)) <= CONSTANTS.eps:
+            return x_dash_k_next
+        x_dash_k = x_dash_k_next
         itr += 1
     return [0, 0]
 
@@ -56,33 +56,33 @@ def g(x, y):
     return g if g != 0 else 0
 
 def helper_func(x, y):
-    return f(x, y) + (-CONSTANTS.r_0 * np.log(-g(x,y)))
+    return f(x, y) + (-CONSTANTS.r_k * np.log(-g(x,y)))
 
 def penalty_func(x, y):
-    return -CONSTANTS.r_0 * np.log(-g(x,y))
+    return -CONSTANTS.r_k * np.log(-g(x,y))
 
 def calculate():
     k = 0
-    x_k = [0.8, -0.3] 
+    x_dash_k = [0.8, -0.3] 
     
     results = [] 
 
     while True:
-        x_dash_dot = cauchy_method(helper_func, x_k) #функция безусловного минимума
+        x_showflake_dot = cauchy_method(helper_func, x_dash_k) # функция безусловного минимума
 
-        results.append([k+1, x_k[0], x_k[1], x_dash_dot[0], x_dash_dot[1], CONSTANTS.r_0])
+        results.append([k+1, x_dash_k[0], x_dash_k[1], x_showflake_dot[0], x_showflake_dot[1], CONSTANTS.r_k])
 
-        x_k = x_dash_dot
+        x_dash_k = x_showflake_dot
 
-        if g(*x_k) <= 0:
-            if abs(penalty_func(*x_k)) <= CONSTANTS.eps:
+        if g(*x_dash_k) <= 0:
+            if abs(penalty_func(*x_dash_k)) <= CONSTANTS.eps:
                 print("\nТаблица результатов:")
-                print(tabulate(results, headers=["Итерация", "x_k[0]", "x_k[1]", "x_dash_dot[0]", "x_dash_dot[1]", "r_k"], tablefmt="grid"))
-                print(f"\nОкончательная точка: {x_k[0]:8.5f} {x_k[1]:8.5f}")
+                print(tabulate(results, headers=["Итерация", "x_dash_k[0]", "x_dash_k[1]", "x_showflake_dot[0]", "x_showflake_dot[1]", "r_k"], tablefmt="grid"))
+                print(f"\nОкончательная точка: {x_dash_k[0]:8.5f} {x_dash_k[1]:8.5f}")
                 print(f"Количество итераций: {k+1}")
                 return
             else:
-                CONSTANTS.r_0 /= CONSTANTS.C
+                CONSTANTS.r_k /= CONSTANTS.C
                 k += 1
 
 if __name__ == "__main__":
--- lab3/mo3Pen.py
+++ lab3/mo3Pen.py
@@ -3,7 +3,7 @@ from tabulate import tabulate
 
 class CONSTANTS:
     eps = 1e-4 
-    r_0 = 1 # начальное значение параметра штрафа 
+    r_k = 1 # начальное значение параметра штрафа 
     C = 8 # для увеличения штрафа
     alpha = 0.1
     beta = 0.8
@@ -19,31 +19,31 @@ def gradient(func, x, y, h=1e-4):
     df_dy = (func(x, y + h) - func(x, y - h)) / (2 * h)
     return [df_dx, df_dy]
 
-def backtracking_line_search(func, x_k, grad, initial_lambda=10.0):
+def backtracking_line_search(func, x_dash_k, grad, initial_lambda=10.0):
     lambda_ = initial_lambda
-    f_x_k = func(x_k[0], x_k[1])
+    f_x_dash_k = func(x_dash_k[0], x_dash_k[1])
 
     while True:
-        x_k_new = vector_sub(x_k, vector_mul(grad, lambda_))
-        f_x_k_new = func(x_k_new[0], x_k_new[1])
+        x_dash_k_new = vector_sub(x_dash_k, vector_mul(grad, lambda_))
+        f_x_dash_k_new = func(x_dash_k_new[0], x_dash_k_new[1])
         grad_norm_sq = sum(g ** 2 for g in grad)
 
-        if f_x_k_new <= f_x_k - CONSTANTS.alpha * lambda_ * grad_norm_sq:
+        if f_x_dash_k_new <= f_x_dash_k - CONSTANTS.alpha * lambda_ * grad_norm_sq:
             break
         lambda_ *= CONSTANTS.beta
     return lambda_
 
 def cauchy_method(func, x):
-    x_k = x[:]
+    x_dash_k = x[:]
     itr = 0
     while itr < 100:
-        grad = gradient(func, x_k[0], x_k[1])
-        lambda_ = backtracking_line_search(func, x_k, grad)
-        x_k_next = vector_sub(x_k, vector_mul(grad, lambda_))
+        grad = gradient(func, x_dash_k[0], x_dash_k[1])
+        lambda_ = backtracking_line_search(func, x_dash_k, grad)
+        x_dash_k_next = vector_sub(x_dash_k, vector_mul(grad, lambda_))
 
-        if abs(func(x_k_next[0], x_k_next[1]) - func(x_k[0], x_k[1])) <= CONSTANTS.eps:
-            return x_k_next
-        x_k = x_k_next
+        if abs(func(x_dash_k_next[0], x_dash_k_next[1]) - func(x_dash_k[0], x_dash_k[1])) <= CONSTANTS.eps:
+            return x_dash_k_next
+        x_dash_k = x_dash_k_next
         itr += 1
 
     return [0, 0]
@@ -62,33 +62,33 @@ def g_plus_vector_func_cut(x, y):
 
 def helper_func(x, y):
     g = g_vector(x, y)
-    return f(x, y) + (CONSTANTS.r_0 / 2) * (g**2 + g_plus_vector_func_cut(x, y)**2)
+    return f(x, y) + (CONSTANTS.r_k / 2) * (g**2 + g_plus_vector_func_cut(x, y)**2)
 
 def penalty_func(x, y):
     g = g_vector(x, y)
-    return (CONSTANTS.r_0 / 2) * (g**2 + g_plus_vector_func_cut(x, y)**2)
+    return (CONSTANTS.r_k / 2) * (g**2 + g_plus_vector_func_cut(x, y)**2)
 
 def calculate():
     k = 0
-    x_k = [-4, -2.5] # начальная точка вне области допустимыx значений
+    x_dash_k = [-4, -2.5] # начальная точка вне области допустимыx значений
 
     results = []
 
     while True:
-        x_dash_dot = cauchy_method(helper_func, x_k)
+        x_showflake_dot = cauchy_method(helper_func, x_dash_k)
 
-        results.append([k+1, x_k[0], x_k[1], x_dash_dot[0], x_dash_dot[1], CONSTANTS.r_0])
+        results.append([k+1, x_dash_k[0], x_dash_k[1], x_showflake_dot[0], x_showflake_dot[1], CONSTANTS.r_k])
         
-        x_k = x_dash_dot
+        x_dash_k = x_showflake_dot
 
-        if penalty_func(x_k[0], x_k[1]) <= CONSTANTS.eps: 
+        if penalty_func(x_dash_k[0], x_dash_k[1]) <= CONSTANTS.eps: 
             print("\nТаблица результатов:")
-            print(tabulate(results, headers=["Итерация", "x_k[0]", "x_k[1]", "x_dash_dot[0]", "x_dash_dot[1]", "r_k"], tablefmt="grid"))
-            print(f"\nОкончательная точка: {x_k[0]:8.4f} {x_k[1]:8.4f}")
+            print(tabulate(results, headers=["Итерация", "x_dash_k[0]", "x_dash_k[1]", "x_showflake_dot[0]", "x_showflake_dot[1]", "r_k"], tablefmt="grid"))
+            print(f"\nОкончательная точка: {x_dash_k[0]:8.4f} {x_dash_k[1]:8.4f}")
             print(f"Количество итераций: {k+1}")
             return
         else: 
-            CONSTANTS.r_0 *= CONSTANTS.C 
+            CONSTANTS.r_k *= CONSTANTS.C 
             k += 1
 
 if __name__ == "__main__":
